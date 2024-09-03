0d4701bd2f4bc2cdda460bdb76011228cf9d2db7
Li Heng <133302251+symxll@users.noreply.github.com>
2024-09-04 02:00:20 +0800

    perf
    There are route and reshaped component.

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,18 +2,19 @@
 
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from '../views/Home/Home';
-import Login from '../views/Login/Login';
-import Layout from '../views/Layout/Layout';
+import Home from '../views/Home';
+import Login from '../views/Login';
 
 const AppRouter: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="login" element={<Login />} />
-        </Route>
+        {/* 留给自定义layout用的 */}
+        {/* <Route path="/" element={<Layout />}> */}
+          <Route index element={<Login />} />
+          <Route path='login' element={<Login />} />
+          <Route path="home" element={<Home />} />
+        {/* </Route> */}
       </Routes>
     </Router>
   );
