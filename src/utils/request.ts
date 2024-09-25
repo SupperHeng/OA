3dc2bcb80f5cad4417f33e81248d3bc35aebf682
Li Heng <133302251+symxll@users.noreply.github.com>
2024-09-26 02:03:54 +0800

    added network request and recoil
    added network request and install recoil

---
+++ src/utils/request.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+const request = axios.create({
+  baseURL: 'http://www.fmin-courses.com:4106',
+  timeout: 3000,
+  headers: {'Content-Type': 'application/json'}
+})
+
+request.interceptors.request.use( config => {
+  // const satoken = Cookies.get('satoken');
+  // if(satoken) config.headers['Authorization'] = `satoken ${satoken}`;
+  return config;
+}, error => {
+  return Promise.reject(error);
+});
+
+request.interceptors.response.use(response => {
+  return response.data;
+}, error => {
+  return Promise.reject(error);
+
+})
+
+export default request;
