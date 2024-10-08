3d64d5e5e3aea3b7056b4770b58991bbfd312c6c
李衡 <symxll@126.com>
2024-10-08 11:53:04 +0800

    New expansion menu

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,11 +1,36 @@
 // src/layout/index.tsx
 
-import React from 'react';
+import React, { useState } from 'react';
 import { Outlet } from 'react-router-dom';
-import { View, MenuItem } from 'reshaped';
+import { View, MenuItem, Button } from 'reshaped';
 import { topMenus, leftMenus } from './menus';
+import { ChevronDown, ChevronUp } from 'react-feather';
 
 const Layout: React.FC = () => {
+  // 组件展开收缩
+  const [show, setShow] = useState(false);
+
+  // 展示菜单
+  const viewItem = () => {
+    setShow(!show);
+  }
+
+  // 菜单组件
+  const showItem = leftMenus.map(item => (
+    item.children ? (
+      <>
+        <Button key={item.label} color='primary' endIcon={show ? ChevronDown : ChevronUp} onClick={viewItem}>{item.label}</Button>
+        { 
+          show && item.children.map(child => (
+            <MenuItem key={child.label} href={child.path}>{child.label}</MenuItem>
+          ))
+        }
+      </>
+    ) : (
+      <MenuItem key={item.label} href={item.path}>{item.label}</MenuItem>
+    )
+  ))
+
   return (
     <View width="100vw" height="100vh" gap={3}>
       <View direction="column">
@@ -20,20 +45,16 @@ const Layout: React.FC = () => {
           }
         </View>
         {/* Menus */}
-        <View direction="row" width="100vw" backgroundColor='page'>
+        <View direction="row" width="100vw">
           <View direction="column" gap={3} minHeight={200} width={'264px'}>
-            {
-              leftMenus.map((item, index) => (
-                <MenuItem key={index} href={item.path}>{item.label}</MenuItem>
-              ))
-            }
+            { showItem }
           </View>
+          
           {/* Main */}
           <View width={"calc(100% - 264px)"} minHeight={200}>
             <Outlet />
           </View>
-          {/* Footer */}
-          {/* <View width="100vw" height={20} backgroundColor='critical'></View> */}
+        
         </View>
       </View>
     </View>
--- src/layout/menus.ts
+++ src/layout/menus.ts
@@ -13,11 +13,32 @@ const topMenus = [
 
 const leftMenus = [
   {
-    label: 'Home Page',
-    path: '/home',
+    label: '展开菜单',
+    children: [
+      {
+        label: '展开1',
+        path: './'
+      },
+      {
+        label: '展开2',
+        path: './'
+      },
+      {
+        label: '展开3',
+        path: './'
+      },
+    ]
   },
   {
-    label: 'Themes Page',
+    label: 'Themes1',
+    path: '/themes',
+  },
+  {
+    label: 'Themes 12321Page',
+    path: '/themes',
+  },
+  {
+    label: 'Themes 213Page',
     path: '/themes',
   },
 ]
